44e9075fd4974b464e7b31fda3caf3d83a8f5cff
Brad Warren <bmw@eff.org>
2018-01-04 18:35:46 -0800

    improve simple_https_server

--- tools/simple_https_server.py
+++ tools/simple_https_server.py
@@ -4,16 +4,20 @@
 If arguments are provided on the command line, the first argument is considered
 to be the port to use and the second is a file containing both the server's
 private key and certificate in PEM format. If these arguments are not provided,
-port 8443 is used with server.pem from the current working directory.
+a random port is used with server.pem from the current working directory.
 
 """
 import BaseHTTPServer, SimpleHTTPServer
 import ssl
 import sys
 
-port = sys.argv[1] if sys.argv[1:] else 8443
+port = int(sys.argv[1]) if sys.argv[1:] else 0
 certfile = sys.argv[2] if sys.argv[2:] else './server.pem'
 
 server = BaseHTTPServer.HTTPServer(('localhost', port), SimpleHTTPServer.SimpleHTTPRequestHandler)
 server.socket = ssl.wrap_socket(server.socket, certfile=certfile, server_side=True)
+
+print "Serving HTTPS on {0} port {1} ...".format(*server.server_address)
+sys.stdout.flush()
+
 server.serve_forever()
